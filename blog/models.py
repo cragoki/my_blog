7fd4ef574ea1e9bc0b8ddfb8de591b19c4e46556
Craig <craig.rodger@bsw.co.uk>
2025-02-18 14:25:38 +0000

    Implementing Data Models
    Implementing Data Models and adjusting the view functions to no longer use the template data, but to retrieve data from the database instead

--- blog/models.py
+++ blog/models.py
@@ -6,10 +6,19 @@ from django.db import models
 class Tag(models.Model):
     caption =models.CharField(max_length=20)
 
+    def __str__(self):
+        return self.caption
+
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
     email_address = models.EmailField()
+
+    def full_name(self):
+        return "%s %s" % (self.first_name, self.last_name)
+
+    def __str__(self):
+        return self.full_name()
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -20,3 +29,4 @@ class Post(models.Model):
     content = models.TextField(validators=[MinLengthValidator(10)])
     author = models.ForeignKey(Author, on_delete=models.SET_NULL, related_name='posts', null=True)
     tags = models.ManyToManyField(Tag)
+

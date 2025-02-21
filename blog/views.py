acfe764f1d9bafdc2914d4232bdf6afebe4ae984
Craig <craig.rodger@bsw.co.uk>
2025-02-21 16:28:36 +0000

    Class Based Views
    Converting function views to class based views

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,56 @@
+
 from django.shortcuts import render, get_object_or_404
+from django.views.generic import ListView, DetailView
 
 from blog.models import Post
 
-# Create your views here.
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3] #- is desc
+
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]  # Order By Date Descending
+    context_object_name = "posts"
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
+
+
+#def starting_page(request):
+    #latest_posts = Post.objects.all().order_by("-date")[:3]  # - is desc
     #sorted_posts = sorted(all_posts, key=get_date)
-    #latest_posts = sorted_posts[-3:] #end of the list, move three left, to the end of the list
-    return render(request, "blog/index.html", {
-        "posts": latest_posts
-    })
+    #latest_posts = sorted_posts[-3:]  # end of the list, move three left, to the end of the list
+    #return render(request, "blog/index.html", {
+        #"posts": latest_posts
+    #})
 
-def posts(request):
-    all_posts = Post.objects.all().order_by("-date")
-    return render(request, "blog/all-posts.html",
-                  {
-                      "all_posts": all_posts
-                  })
+class AllPostsView(ListView):
+    template_name = "blog/all-posts.html"
+    model = Post
+    ordering = ["-date"]  # Order By Date Descending
+    context_object_name = "all_posts"
 
-def post_detail(request, slug):
-    identified_post = get_object_or_404(Post,slug=slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+
+#def posts(request):
+    #all_posts = Post.objects.all().order_by("-date")
+    #return render(request, "blog/all-posts.html",
+                  #{
+                      #"all_posts": all_posts
+                  #})
+
+class PostDetailView(DetailView):
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+
+#def post_detail(request, slug):
+    #identified_post = get_object_or_404(Post, slug=slug)
+    #return render(request, "blog/post-detail.html", {
+        #"post": identified_post,
+        #"post_tags": identified_post.tags.all()
+    #})
